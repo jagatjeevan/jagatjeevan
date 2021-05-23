4df0bdad64b29124244b344b8f5386bfacb1f27c
Jagat Jeevan Sahoo <jagatsah@thoughtworks.com>
2021-05-23 14:31:05 +0530

    Add Homepage

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,54 @@
+import React, { useEffect, useState } from "react";
 import Footer from "../src/components/Footer";
 import Header from "../src/components/Header";
 
-import styles from "../styles/Home.module.css";
+import styles from "../styles/Home.module.scss";
+
+const GetTalent = () => {
+  const [index, setIndex] = useState(0);
+
+  const talent = [
+    "Frontend Development",
+    "Responsive sites",
+    "Progressive Web Apps",
+    "Mailer",
+  ];
+
+  const getTalentText = () => {
+    return <span className={styles.talent}>{talent[index]}</span>;
+  };
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      if (index === talent.length - 1) {
+        setIndex(0);
+      } else {
+        setIndex(index + 1);
+      }
+      getTalentText();
+    }, 3000);
+    return () => clearInterval(timer);
+  }, [index]);
+
+  return getTalentText();
+};
 
 export default function Home() {
   return (
-    <div className={styles.layout}>
+    <article className={styles.layout}>
       <Header />
-      <div>Here goes the front page</div>
+      <section className={styles.introContainer}>
+        <div className={styles.intro}>
+          <p>Hey there!</p>
+          <p>I am Jagat Jeevan.</p>
+          <p>
+            And I do <GetTalent />
+          </p>
+        </div>
+        <img src="/profile.jpeg" className={styles.rounded} />
+      </section>
+
       <Footer />
-    </div>
+    </article>
   );
 }
