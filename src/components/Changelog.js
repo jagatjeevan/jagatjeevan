3d6bc8aeb2302d1811c3e45d4cfdde9ca1dc9a8b
Jagat Jeevan Sahoo <kanhajeevan@gmail.com>
2025-05-08 17:53:02 +0530

    test commit for change log

---
+++ src/components/Changelog.js
@@ -0,0 +1,30 @@
+function Changelog() {
+  const repoOwner = 'facebook';
+  const repoName = 'react';
+
+  async function fetchReleases() {
+    const response = await fetch(`https://api.github.com/repos/${repoOwner}/${repoName}/releases`);
+    const releases = await response.json();
+    const changelogDiv = document.getElementById('changelog');
+
+    releases.forEach((release) => {
+      const releaseDiv = document.createElement('div');
+      releaseDiv.innerHTML = `
+            <h2>${release.name} - ${new Date(release.published_at).toLocaleDateString()}</h2>
+            <p>${release.body}</p>
+          `;
+      changelogDiv.appendChild(releaseDiv);
+    });
+  }
+
+  fetchReleases();
+
+  return (
+    <>
+      <h1>Changelog</h1>
+      <div id="changelog"></div>
+    </>
+  );
+}
+
+export default Changelog;
